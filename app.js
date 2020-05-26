448ebd5b23a3cab794fd18a4bacff97901226f37
Chaitanya Patil <chaitanya.patil@sakec.ac.in>
2020-05-26 20:09:45 +0530

    error handling

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const app = express();
 
+
+//Middleware 
+const morgan = require('morgan');
+
+//importing routes
 const productRoutes = require('./bin/routes/products');
 const orderRoutes = require('./bin/routes/orders');
 
+
+//using middleware
+app.use(morgan('dev'));
+
+// using routes
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+
+//error ahndling
+
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+  });
+  
+  app.use((error, req, res, next) => {
+    res.status(error.status);
+    res.json({
+      error: {
+        message: error.message
+      }
+    });
+  });
 
 module.exports = app;
